73703f1d19c610d5404f20fb6138bcb19e80d881
naertz <naertz@dmacc.edu>
2021-12-11 19:07:34 -0600

    Added test for view responses

--- crypto_graphs/tests.py
+++ crypto_graphs/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
+from . import views
+
 # Create your tests here.
+
+
+class ViewsTestCase(TestCase):
+    def test_load_views(self):
+        request = self.client.request()
+        atari_response = views.atari_view(request)
+        bitcoin_response = views.bitcoin_view(request)
+        bitcoin_cash_response = views.bitcoin_cash_view(request)
+        curve_dao_token_response = views.curve_dao_token_view(request)
+        ethereum_response = views.ethereum_view(request)
+        fantom_response = views.fantom_view(request)
+        monero_response = views.monero_view(request)
+        tarot_response = views.tarot_view(request)
+        tomb_response = views.tomb_view(request)
+        self.assertEqual(atari_response.status_code, 200)
+        self.assertEqual(bitcoin_response.status_code, 200)
+        self.assertEqual(bitcoin_cash_response.status_code, 200)
+        self.assertEqual(curve_dao_token_response.status_code, 200)
+        self.assertEqual(ethereum_response.status_code, 200)
+        self.assertEqual(fantom_response.status_code, 200)
+        self.assertEqual(monero_response.status_code, 200)
+        self.assertEqual(tarot_response.status_code, 200)
+        self.assertEqual(tomb_response.status_code, 200)
